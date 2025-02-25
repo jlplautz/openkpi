15ea822bab47f1c9b5312e9b3b9b90ed7435def2
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2025-02-25 17:17:31 -0300

    implementação 25fev

--- xml_parse.py
+++ xml_parse.py
@@ -3,14 +3,19 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('20FEV22H00V1.xml')
 root = tree.getroot()
 print(root.tag)
-print(root[0].tag)
+# print(root[0].tag)
 # print(root[0].text)
+# for child in root:
+#     print(child.tag, child.attrib)
 
-print(root.attrib)
+for movie in root.findall("./OMeS/PMSetup/PMMOResult/NE-WBTS_1.0/[measurementType='SBTS_Energy_Consumption']"):
+    print(movie.attrib)
 
 
-for elem in tree.iter():
-    print(elem.tag, elem.text)
+# print(root.attrib)
+
+# for elem in tree.iter():
+#     print(elem.tag, elem.text)
 
 
 # for PMSetup in root.iter('PMSetup'):
@@ -20,8 +25,30 @@ for elem in tree.iter():
 #     print(MO.attrib)
 
 
-# for NEWBTS in root.iter('NE-WBTS_1.0'):
-#     print(NEWBTS.attrib)
+for NEWBTS in root.iter('NE-WBTS_1.0'):
+    print(NEWBTS.attrib)
 
 # for ltetload in root.findall('PMMOResult'):
 #     print(ltetload.text)
+
+
+# import xml.etree.ElementTree as ET
+
+# root = ET.fromstring(countrydata)
+
+# # Top-level elements
+# root.findall(".")
+
+# # All 'neighbor' grand-children of 'country' children of the top-level
+# # elements
+# root.findall("./country/neighbor")
+
+# # Nodes with name='Singapore' that have a 'year' child
+# root.findall(".//year/..[@name='Singapore']")
+
+# # 'year' nodes that are children of nodes with name='Singapore'
+# root.findall(".//*[@name='Singapore']/year")
+
+# # All 'neighbor' nodes that are the second child of their parent
+# root.findall(".//neighbor[2]")
+
